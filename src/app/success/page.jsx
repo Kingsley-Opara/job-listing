eb7bf7e21d05adc4dc56e9b892bfe24d6f11584b
kingsley <oparaudochukwu277@gmail.com>
2025-03-29 17:31:00 +0000

    fixed an error during deployment

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { GlobalStateProvider } from '../context'
+
 
 function Page() {
   return (
     <>
+      <GlobalStateProvider>
         <Navbar/>
         <div className="flex items-center justify-center h-screen">
-        <h1 className="text-green-500 text-2xl font-bold">Form Submitted Successfully!</h1>
+          <h1 className="text-green-500 text-2xl font-bold">Form Submitted Successfully!</h1>
         </div>
+      </GlobalStateProvider>
     </>
   )
 }
